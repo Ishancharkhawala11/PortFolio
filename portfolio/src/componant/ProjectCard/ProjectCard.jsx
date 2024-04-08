ca15937925cdd343a4e7735e0951e6a64e9631da
Ishancharkhawala11 <ishankcharkhawala@gmail.com>
2024-04-08 20:01:00 +0530

    float

--- portfolio/src/componant/ProjectCard/ProjectCard.jsx
+++ portfolio/src/componant/ProjectCard/ProjectCard.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import "./ProjectCard.css"
 const ProjectCard = ({title,icon}) => {
   return (
-    <div className='ProjectCard'>
+    <div className='ProjectCard floating'>
      <img src={icon} alt='icon'/>
      <h1>{title}</h1>
        

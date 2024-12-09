0d72ad6778686a2a670ac246470d8181bce7f74d
Ishancharkhawala11 <156002746+Ishancharkhawala11@users.noreply.github.com>
2024-12-09 15:27:28 +0530

    Update Hero.jsx

--- portfolio/src/componant/Hero/Hero.jsx
+++ portfolio/src/componant/Hero/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
         <img src={user} alt={user} className="user"></img>
         <h1 className="welcome">Welcome to my portfolio!</h1>
         <div className="text"> <p>
-        I'm Ishan Charkhawala, a budding UI/UX developer and web developer with a passion for creating engaging digital experiences. Though I'm relatively new to the industry, my enthusiasm and dedication drive me to continually learn and grow in my craft.
+        I'm Ishan Charkhawala, a budding Flutter developer  with a passion for creating engaging digital experiences. Though I'm relatively new to the industry, my enthusiasm and dedication drive me to continually learn and grow in my craft.
         </p></div>
        
       </div>
